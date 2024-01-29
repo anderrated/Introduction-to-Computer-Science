f170335a4d72e3a1f9696c986a3699eab8fbee84
hotgoddess <aalaserna@up.edu.ph>
2024-01-29 14:44:40 +0800

    factorial submitted lab3

--- lab3_xfactorial.py
+++ lab3_xfactorial.py
@@ -8,12 +8,16 @@ EXERCISE # 2 - The X Factorial
 Write a function that solves for the factorial of an integer n. The factorial of a number is a sequential 
 multiplication operation of all the natural numbers that are less than it. 
 '''
+def main():
+    n = int(input("Enter a number 'n': "))
+    print(f"The factorial of {n} is {factorial(n)}")
 
-n = int(input("Enter a number 'n': "))
-
-# Base cases
-if (n == 0 or n == 1):
-    print(f"The factorial of {n} is 1")
-else:
-# Recursive case
-    print(f"The factorial of {n} is {n} * {n-1} = {n * (n-1)}")
+def factorial(n):
+    # Base cases
+    if (n == 0 or n == 1):
+        return 1
+    else:
+    # Recursive case
+        return (n * factorial(n-1))
+    
+main()
