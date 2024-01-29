fc246ea5acb93c166ec530c88689bfa25ac5011e
hotgoddess <aalaserna@up.edu.ph>
2024-01-29 13:38:00 +0800

    Added lab2

---
+++ lab2_upv.py
@@ -0,0 +1,13 @@
+# Author : Andrea Laserna
+# Created on October 9, 2022
+# Modified on January 29, 2024
+
+print("HELLO KOMSAI!")
+print("WELCOME TO UPV!!!")
+print (f'{"+": <11} +',           f'{"+ - - - - -": <10} +',   f'{"+": <11} +')
+print (f'{"|": <11} |',           f'{"|": <11} |',             f'{"+": <11} +')
+print (f'{"|": <11} |',           f'{"|": <11} |',             f'{"+": <11} +')  
+print (f'{"|": <11} |',           f'{"+ - - - - -": <10} +',   f'{"  +": <9} +')
+print (f'{"|": <11} |',           f'{"|": <11} ',              f'{"     +": <8} +')
+print (f'{"+ - - - - -": <10} +', f'{"|": <11} ',              "       +")
+print("See you in the world of Python! :)")
