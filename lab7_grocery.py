3730a51b75032824b678ae9394e1c50777720390
hotgoddess <aalaserna@up.edu.ph>
2024-01-29 18:13:39 +0800

    lab7 instructions

--- lab7_grocery.py
+++ lab7_grocery.py
@@ -2,3 +2,7 @@
 # Created on November 25, 2022
 # Modified on January 29, 2024
 
+'''
+A mother wants to list down the things she needs to buy, however, she needs a simple list that be run 
+every time and can be modified whenever she changes her mind.
+'''
