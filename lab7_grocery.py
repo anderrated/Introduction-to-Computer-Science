d97455670e924504c085f8350a9db8fcfd059f3f
hotgoddess <aalaserna@up.edu.ph>
2024-01-29 18:15:14 +0800

    added instructions

--- lab7_grocery.py
+++ lab7_grocery.py
@@ -5,4 +5,11 @@
 '''
 A mother wants to list down the things she needs to buy, however, she needs a simple list that be run 
 every time and can be modified whenever she changes her mind.
+
+Starting with just an empty list, write a function that creates a grocery list that does the following:
+• Add an Item (takes a string input from the user and add it to the existing list)
+• Remove an Item (takes a string input from the user and removes all instances from the list)
+• Print entire list (prints out all the contents of the list)
+• Exit (exit the program)
+
 '''
