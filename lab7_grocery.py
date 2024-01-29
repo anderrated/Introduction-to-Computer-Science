7663b647f94d0481575d69b299d72b7d15db3c1e
Andrea Laserna <andrealaserna1@gmail.com>
2024-01-29 18:47:09 +0800

    lab7 grocery added

--- lab7_grocery.py
+++ lab7_grocery.py
@@ -12,3 +12,52 @@ Starting with just an empty list, write a function that creates a grocery list t
 • Print entire list (prints out all the contents of the list)
 • Exit (exit the program)
 '''
+
+def main():
+    grocery_list = []
+    run = True
+    print("    GROCERY LIST    ")
+    print("====================")
+
+    while run:
+        print("What would you like to do?")
+        print("1. Add an item")
+        print("2. Remove an item")
+        print("3. Print entire list")
+        print("4. Exit\n")
+        choice = input("Choice (1-4): ")
+        print("====================")
+
+        if choice == "1":
+            add_item(grocery_list)
+        elif choice == "2":
+            remove_item(grocery_list)
+        elif choice == "3":
+            print_list(grocery_list)
+        elif choice == "4":
+            print("Thank you for using this program!")
+            break
+        else:
+            print("Invalid input. Please try again.")
+
+def add_item(grocery_list):
+    item = input("Enter the item to add: ")
+    grocery_list.append(item)
+    print("====================\n")
+
+def remove_item(grocery_list):
+    item_to_remove = input("Enter the item to remove: ")
+    for item in grocery_list:
+        if item_to_remove.lower() == item.lower():
+            grocery_list.remove(item)
+        else:
+            print("Item not found.")
+    print("====================\n")
+
+def print_list(grocery_list):
+    print("PRINTING LIST...\n")
+    for item in grocery_list:
+        print(item)
+    print("====================\n")
+
+main()
